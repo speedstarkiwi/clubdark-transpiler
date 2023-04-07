ecfb5da40fb9c34dccbecaf5590f4527a5db5efe
speedstarkiwi <121459772+speedstarkiwi@users.noreply.github.com>
2023-04-07 12:31:38 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,11 @@
-HANDLE sub_10028DA0()
+//skidded and gay calamari
+//imperious gon fin be mad lmaoo
+///gay comments btw
+HANDLE main()//lol gay ass
 {
-  HMODULE v0; // esi
-  HMODULE v1; // eax
-  int v2; // eax
-  int **v3; // eax
-  int *v4; // ebx
-  int *v5; // edi
-  _BYTE *v6; // eax
-  const char *v7; // ecx
-  unsigned int v8; // esi
-  bool v9; // cf
-  int v10; // ecx
-  int v11; // eax
-  int v12; // edi
-  int (__cdecl **v13)(int); // ebx
-  const char *v14; // eax
-  size_t v15; // ebx
-  int *v16; // esi
-  int v17; // eax
-  unsigned __int16 v18; // cx
-  int v19; // ecx
-  int (__cdecl *v20)(int); // eax
-  int v21; // edi
-  HMODULE v22; // esi
-  int i; // ebx
-  int v25; // [esp+4h] [ebp-38h] BYREF
-  int v26; // [esp+8h] [ebp-34h]
-  int v27; // [esp+14h] [ebp-28h]
-  int v28; // [esp+18h] [ebp-24h]
-  char v29; // [esp+1Ch] [ebp-20h]
-  void *v30; // [esp+2Ch] [ebp-10h] BYREF
-  int v31; // [esp+30h] [ebp-Ch]
-  int v32; // [esp+34h] [ebp-8h]
-
   v0 = GetModuleHandleA(0) + 3072164;
   v1 = GetModuleHandleA(0);
-  v2 = ((int (__stdcall *)(int *))(v1 + 3072080))(&dword_100348C8);
+  v2 = ((int (__stdcall *)(int *))(v1 + 3072080))(&dword_100348C8);//omg hes using type defs!
   ((void (__thiscall *)(int))v0)(v2);
   dword_10034870 = dword_100348C8 + 12;
   v3 = *(int ***)(dword_100348C8 + 56);
@@ -43,10 +13,10 @@ HANDLE sub_10028DA0()
   v5 = *v3;
   if ( *v3 != v4 )
   {
-    while ( 2 )
+    while ( 2 )//?
     {
       v6 = (_BYTE *)(*(int (**)(void))(*(_DWORD *)*v5 + 16))();
-      v7 = "ScriptContext";
+      v7 = "ScriptContext";//lmao skid ass nigga
       v8 = 9;
       while ( *(_DWORD *)v6 == *(_DWORD *)v7 )
       {
@@ -97,7 +67,7 @@ LABEL_8:
     }
     else
     {
-      *(_DWORD *)(*(_DWORD *)(v12 + 8) + 8) = 0;
+      *(_DWORD *)(*(_DWORD *)(v12 + 8) + 8) = 0;//assuming its lua state
     }
     *(_DWORD *)(v12 + 8) += 16;
     v18 = *(_WORD *)(v12 + 52) + 1;
@@ -105,7 +75,7 @@ LABEL_8:
     if ( v18 >= 0xC8u )
     {
       if ( v18 == 200 )
-        sub_1000E9F0(v12, "C stack overflow", v29);
+        sub_1000E9F0(v12, "C stack overflow", v29);//bro did not embed lua
       if ( v18 >= 0xE1u )
         sub_1000EBC0(v12, 5);
     }
@@ -124,7 +94,8 @@ LABEL_8:
   v31 = -858993460;
   v22 = GetModuleHandleA(0);
   v32 = -858993460;
-  for ( i = 0; i < 2147483645; ++i )
+  ///gay callcheck
+  for ( i = 0; i < 2147483645; ++i )//whats he checking?
   {
     if ( v21 == 1000 )
       break;
@@ -144,35 +115,30 @@ LABEL_8:
     }
     v22 = (HMODULE)((char *)v22 + 1);
   }
-  AddVectoredExceptionHandler(1u, Handler);
-  v28 = (int)"game";
+  AddVectoredExceptionHandler(1u, Handler);//skidded callcheck real
+  v28 = (int)"game";//gay getglobal
   *(_DWORD *)(*(_DWORD *)(dword_10034868 + 116) + 24) = 7;
+  //shit ass luac nigga
   dword_100348B0(v28);
   dword_100348B0("GetService");
   dword_100348A4(dword_10034868, -2);
   dword_100348A8(dword_10034868, "RunService");
   dword_100348A0(dword_10034868, 2, 1, 0);
   dword_100348B0("RenderStepped");
-  dword_100348B0("Connect");
+  dword_100348B0("Connect");//calamari stop playing with me :Sob:
   dword_100348A4(dword_10034868, -2);
   dword_100348B4(dword_10034868, sub_10028990, 0, 1, 0);
   dword_100348A0(dword_10034868, 2, 0, 0);
-  *(_DWORD *)(dword_10034868 + 16) -= 32;
-  v27 = 0;
-  v28 = 15;
-  LOBYTE(v25) = 0;
-  sub_10002530(
-    &v25,
+  *(_DWORD *)(dword_10034868 + 16) -= 32;///decrementing the top?
+  execute(
+    &v25,//get objects but shit
     "function GetObjects(assetId) local obj = game:GetService(\"InsertService\"):LoadLocalAsset(assetId) return { obj }; end",
     0x75u);
-  sub_10028590(v25, v26);
-  v27 = 0;
-  v28 = 15;
-  LOBYTE(v25) = 0;
+
   sub_10002530(
-    &v25,
+    &v25,//gay watermark
     "game.StarterGui:SetCore('SendNotification', {Duration = 10; Title='Club Dark'; Text='Injected'; Icon = 'rbxassetid://2935928760'})",
     0x82u);
   sub_10028590(v25, v26);
-  return CreateThread(0, 0, StartAddress, 0, 0, 0);
+  return CreateThread(0, 0, StartAddress, 0, 0, 0);//make pipe because no life
 }
